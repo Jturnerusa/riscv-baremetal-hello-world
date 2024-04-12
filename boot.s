cc60fd55e17b2ae7fc6bbc244e53df693b9e8a60
John Turner <jturner.usa@gmail.com>
2024-04-12 18:04:16 -0400

    debubbing

--- boot.s
+++ boot.s
@@ -5,14 +5,5 @@ start:
         csrw satp, 0
         la sp, stack_top
         la gp, global_pointer
-
-
-        la t5, bss_start
-        la t6, bss_end
-bss_clear_loop: 
-        sd zero, (t5)
-        addi t5, t5, 8
-        bltu t5, t6, bss_clear_loop
-
         la t0, kmain
         tail kmain
--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,18 @@
 mod kprint;
 mod uart;
 
+use core::ptr;
+
+extern "C" {
+    static bss_start: *const u64;
+    static bss_end: *const u64;
+}
+
 #[no_mangle]
-extern "C" fn kmain() {
+unsafe extern "C" fn kmain() {
+    let end = ptr::read(bss_end);
+    kprintln!("{:?}", end);
+    kprintln!("hello");
     loop {}
 }
 
